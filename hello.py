2df64162caa713844d1f55ac21738c38ee40e759
Prinu Dickson <prinudickson@gmail.com>
2023-10-21 21:51:33 +0200

    create student iunstances who can greet.

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from icecream import ic
+
 class MercedesBenz:
     model = "z"
     weight = 2300
@@ -5,19 +7,43 @@ class MercedesBenz:
     doors = 4
     wheels = 4
 
+    def __init__(self, color="black"):
+        self.color = color
+        
+    def drive(self):
+        ic(f"A Mercedez is driving. It is {self}\n")
 
-print(MercedesBenz.__dict__)
+ic(MercedesBenz.__dict__)
 
 a = MercedesBenz()
 
-print(a.__dict__)
+ic(a.__dict__)
 
 MercedesBenz.color = "Blue"
 
-print(MercedesBenz.__dict__)
+ic(MercedesBenz.__dict__)
 
-print(a.color)
+ic(a.color)
 
-print(dir(a))
+ic(dir(a))
 
+
+m1 = MercedesBenz()
+m2 = MercedesBenz()
+
+ic(m1.__dict__)
+ic(m2.__dict__)
+
+
+objs = [m1, m2]
+
+attribs = ["color", "doors"]
+vals = ["Red",4]
+
+for obj in objs:
+    for attr, val in zip(attribs, vals):
+        setattr(obj, attr, val) 
+
+ic(m1.__dict__)
+ic(m2.__dict__)
 
