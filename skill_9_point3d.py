da18c47d473dcf6ccc867f15ff0b33bc83ba912d
prinudickson ubuntu <prinudickson@gmail.com>
2024-01-15 22:51:45 +0100

    updated after looking at the solution.

--- skill_9_point3d.py
+++ skill_9_point3d.py
@@ -9,21 +9,40 @@ class point3d:
         self.z = z
     
     def __repr__(self) -> str:
-        instance_name = "".join([t.__name__ for t in type(self).__mro__[:1]])
-        return f"{instance_name}({self.x}, {self.y}, {self.z})"
+        class_name = self.__class__.__name__
+        attr_final = ""
+
+        if class_name != "point3d":
+            additional_attr = self.__class__.__dict__["__slots__"]
+            if isinstance(additional_attr, tuple) == 1:
+                attr = additional_attr[0]
+                attr_val = getattr(self, attr)
+            elif isinstance(additional_attr, str):
+                attr = additional_attr
+                attr_val = getattr(self, attr)
+            else:
+                raise AttributeError("Cannot handle this!")
+            
+            attr_final = f", {attr}='{attr_val}'"
+            
+        return f"{class_name}({self.x}, {self.y}, {self.z}{attr_final})"
 
 class coloredPoint(point3d):
-    __slots__ = ("color")
+    #making this slots definition to reflect as tuple
+    #This will be tested in __repr__
+    __slots__ = ("color", )
 
-    def __init__(self, x, y, z, color) -> None:
-        super().__init__(x, y, z)
+    def __init__(self, *args, color="black", **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.color = color
 
 class shapePoint(point3d):
+    #making this slots definition reflect as str
+    #This will be tested in __repr__
     __slots__ = ("shape")
 
-    def __init__(self, x, y, z, shape) -> None:
-        super().__init__(x, y, z)
+    def __init__(self, *args, shape="sphere", **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.shape = shape
 
 if __name__ == "__main__":
@@ -36,4 +55,4 @@ if __name__ == "__main__":
     sp = shapePoint(1,2,3, shape="square")
     ic(sp)
     
-    ic(sp.__dict__)
+    #ic(sp.__dict__)
