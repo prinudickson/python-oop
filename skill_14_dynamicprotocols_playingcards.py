121a10590fa0f97f587a38cb390a99a9d9c9611f
Prinu Dickson <prinu.dickson@pwc.com>
2024-06-09 10:30:31 +0200

    update 14

--- skill_14_dynamicprotocols_playingcards.py
+++ skill_14_dynamicprotocols_playingcards.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from icecream import ic
 from itertools import product
+import random
 
 @dataclass()
 class PlayingCard:
@@ -30,10 +31,43 @@ class CardDeck:
             self._cards = playing_cards
         else:
             #full deck
-            self._cards = product(self.SUITS, self.RANKS)
+            self._cards = [PlayingCard(*args) for args in product(self.SUITS, self.RANKS)]
 
     def __len__(self):
         return len(self._cards)
+    
+    def __getitem__(self, item):
+        if type(item) == slice:
+            return CardDeck(self._cards[item])
+        
+        return self._cards[item]
+    
+    def __add__(self, other):
+        if type(other) == PlayingCard:
+            return CardDeck([*self._cards, other])
+        elif type(other) == CardDeck:
+            return CardDeck([*self._cards, *other._cards])
+        
+    def __radd__(self, other):
+        return self + other
+    
+    def __mul__(self, other):
+        if type(other) == int:
+            return CardDeck(self._cards * other)
+        
+    def __rmul__(self, other):
+        return self * other
+    
+    def draw(self, n=1):
+        drawn_cards = list()
+
+        for i in range(n):
+            idx = random.randrange(len(self))
+            drawn_cards.append(self._cards.pop(idx))
+        #randomly pick an index to draw from. 
+        #take that item out
+        pass #return
+
             
 
 
