98478b13862f4190f39b5bcb6114d7435844c99f
prinudickson ubuntu <prinudickson@gmail.com>
2024-07-07 23:15:31 +0200

    completed initial solution of skill_15

--- skill_15_abc_jobapplication.py
+++ skill_15_abc_jobapplication.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from collections.abc import Sequence
 from icecream import ic
 
+from random import randint, uniform
+
 @dataclass(frozen=True)
 class JobApplicant:
     applicant_id: str
@@ -19,24 +21,50 @@ class JobApplicantPool(Sequence):
     
     def __getitem__(self, item):
         if type(item) == slice:
-            type(self)(*self._applications[item])
+            return type(self)(*self._applicants[item])
         elif type(item) == int:
             return self._applicants[item]
         
         return NotImplemented
     
-    def __len__(self):
-        
-        pass
+    def __len__(self) -> int:
+        return len(self._applicants)
+    
+    def __repr__(self) -> str:
+        sorted_applicants = sorted(self._applicants, key=self.score, reverse=True)
+        header = f"Applicant Pool\n(Score | ID)\n{'-'*20}\n"
+        return header + "\n".join([
+            f"{self.score(applicant)} - {applicant.applicant_id}" for applicant in sorted_applicants
+        ])
 
     @staticmethod
     def score(applicant: JobApplicant):
-        pass
+        return round(
+            applicant.years_experience * 0.5 +
+            applicant.is_recommended +
+            applicant.first_interview_score * 0.5 +
+            applicant.second_interview_score
+            , 2)
 
     
-
+def generate_randon_applicants(n=18):
+    return [
+        JobApplicant(
+            applicant_id=str(randint(10000,90000)),
+            years_experience=randint(0,10),
+            is_recommended=bool(randint(0,1)),
+            first_interview_score=round(uniform(1.0,10.0),2),
+            second_interview_score=round(uniform(1.0,10.0),2)
+        ) for _ in range(n)
+    ]
 
 
 if __name__ == "__main__":
-    jap = JobApplicantPool()
+    jap = JobApplicantPool(*generate_randon_applicants(n=2))
+    ic(jap)
+    ic(len(jap))
+    # ic(jap[2:4])
+
+    # ic(*generate_randon_applicants(n=1))
+    jap.add(*generate_randon_applicants(n=1))
     ic(jap)
